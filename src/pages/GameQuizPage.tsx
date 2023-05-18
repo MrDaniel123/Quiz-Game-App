5d26e5cd7e982d43189519d8e2675dd37b1532c8
Daniel <pandaniel13286@gmail.com>
2023-05-18 17:26:37 +0200

    Add a selected Answer Mechanic

--- src/pages/GameQuizPage.tsx
+++ src/pages/GameQuizPage.tsx
@@ -25,7 +25,7 @@ const GameQuiz = () => {
 
 		const answersObject = actualQuizData[0].questions[questionNumber].answers.map(question => {
 			return {
-				question: question,
+				answer: question,
 				state: 'nonClicked',
 				isCorrectAnswer:
 					question === actualQuizData[0].questions[questionNumber].currentAnswer ? true : false,
@@ -37,11 +37,78 @@ const GameQuiz = () => {
 		setActualQUestion(actualQuizData[0].questions[questionNumber]); //!!Actual questions data
 	}, []);
 
-	// const goToNextQuestionHandler = () => {
-	// 	setQUestionNumber(questionNumber + 1);
-	// };
+	const goToNextQuestionHandler = () => {
+		setQUestionNumber(questionNumber + 1);
+	};
 
 	if (questionData) {
+		const AnswerOnClickHandler = (clickedAnswer: any) => {
+			console.log(clickedAnswer);
+			let isClickedCurrectAnswer;
+
+			actualAnswers.forEach((answer: any) => {
+				if (answer.answer === clickedAnswer) {
+					isClickedCurrectAnswer = true;
+				} else {
+					isClickedCurrectAnswer = false;
+				}
+			});
+
+			const actualQuizData = quizData.filter(actualQuiz => {
+				if (actualQuiz.quizName.replace(/\s/g, '') === quizName) {
+					return actualQuiz;
+				}
+			});
+
+			if (isClickedCurrectAnswer) {
+				const answersObject = actualQuizData[0].questions[questionNumber].answers.map(answer => {
+					if (clickedAnswer === answer) {
+						return {
+							answer: answer,
+							state: 'currentAnswer',
+							isCorrectAnswer:
+								answer === actualQuizData[0].questions[questionNumber].currentAnswer ? true : false,
+						};
+					} else
+						return {
+							answer: answer,
+							state: 'nonClicked',
+							isCorrectAnswer:
+								answer === actualQuizData[0].questions[questionNumber].currentAnswer ? true : false,
+						};
+				});
+				setActualAnswers(answersObject);
+			} else {
+				const answersObject = actualQuizData[0].questions[questionNumber].answers.map(answer => {
+					if (clickedAnswer === answer) {
+						return {
+							answer: answer,
+							state: 'worngAnswer',
+							isCorrectAnswer:
+								answer === actualQuizData[0].questions[questionNumber].currentAnswer ? true : false,
+						};
+					} else
+						return {
+							answer: answer,
+							state: 'nonClicked',
+							isCorrectAnswer:
+								answer === actualQuizData[0].questions[questionNumber].currentAnswer ? true : false,
+						};
+				});
+				setActualAnswers(answersObject);
+			}
+		};
+
+		const renderAnswerButtons = actualAnswers.map((answer: any) => {
+			return (
+				<AnswerButton
+					answer={answer.answer}
+					state={answer.state}
+					onClickEvent={AnswerOnClickHandler}
+				/>
+			);
+		});
+
 		return (
 			<>
 				<Header>{quizName}</Header>
@@ -51,7 +118,8 @@ const GameQuiz = () => {
 						answerNumber={questionNumber + 1}
 						totalAnswer={questionData[0].questions.length}
 					/>
-					{/* <NextQuestionButton onClickHandler={goToNextQuestionHandler} /> */}
+					{renderAnswerButtons}
+					<NextQuestionButton onClickHandler={goToNextQuestionHandler} />
 				</Container>
 			</>
 		);
